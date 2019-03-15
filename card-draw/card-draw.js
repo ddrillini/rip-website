316d98cfb34c70285098d39c00801377e8db6574
Ian Klatzco <iklatzco@gmail.com>
2019-03-15 04:51:38 -0500

    complete 13 yeetening

--- card-draw/card-draw.js
+++ card-draw/card-draw.js
@@ -1,3 +1,8 @@
+// Originally written by Ian.
+// Revamped by Tana.
+// I have opted for very verbose comments with the intent
+// that less-JS-experienced clubmembers may open this in the future.
+
 const tournament = 'rip11';
 
 const discordWebhookUrl = 'https://dis' + 'corda' + 'pp.com/api/webho' + 'oks/51087618717882' + '7786/zBNpG-Db' + 'lnHLqfaHt8' + 'mI1gPZfKr0wEZ5p6MIr' + 'Z1fZ' + 'RJRz8jFYv' + '1IwqNIfdH5xIY-w_Ud'; // so bots don't scrape it
@@ -5,9 +10,11 @@ const previousStateStack = [];
 let cardObjects = [];
 let songs = [];
 
+// when the webpage is finished loading
 $(document).ready(() => {
   const cards = $('#card-area');
 
+  // load the song data
   $.getJSON(`res/${tournament}/data.json`, (data) => {
     songs = data;
   });
@@ -16,6 +23,8 @@ $(document).ready(() => {
 
   let currentPosition = -1;
 
+  // param: number of cards you want
+  // return: array of random integers from set of all possible songs
   function randomize(numRequested) {
     const randomNumberArray = [];
 
@@ -23,10 +32,30 @@ $(document).ready(() => {
     for (let i = 0; i < numRequested; i += 1) {
       // generate a random number
       let x;
+      let condition = true;
       do {
+        // generate a random number on the range 0, total # songs - 1
         x = Math.floor(Math.random() * songs.length);
-        // catch duplicates
-      } while (randomNumberArray.indexOf(x) >= 0);
+
+        // spaghettic code i'm sorry -ian
+        if (document.getElementById("thirteens").checked)
+        {
+          // exit once you find an int not in the list
+          // AND an int that doesn't correspond to a 13
+          let check_if_in_list = randomNumberArray.indexOf(x) >= 0;
+          let check_if_thirteen = songs[x].difficulty == 13 ? true : false ;
+          // if either condition is true, you want to keep looping
+          // i.e. set cond to true
+          condition = check_if_in_list || check_if_thirteen;
+          console.log("yeeted 13 " + songs[x].title);
+        } else {
+          // exit once you find an int not in the list
+          condition = randomNumberArray.indexOf(x) >= 0;
+        }
+      } while (condition);
+
+      // and with a difficulty number that is not 13
+      // songs[x].difficulty != 13
 
       randomNumberArray.push(x);
     }
