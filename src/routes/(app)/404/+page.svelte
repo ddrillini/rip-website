a85bd3da8c0805b760ffa7f1adee5b254a474f9c
Spencer Gunning <csit@cering.dev>
2026-03-11 00:48:21 -0500

    clean up 404 page

--- src/routes/(app)/404/+page.svelte
+++ src/routes/(app)/404/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-  import globals from '../../../globals';
 	import Meta from '../../../components/layout/Meta.svelte';
-	import Action from '../../../components/Action.svelte';
-  import Chart from '../../../components/Chart.svelte';
-
 </script>
 
 <svelte:head>
-  <Meta title="404" desc='The Technical Proficiency Exam 3 at Rumble in the Prairie 15.' noindex={false}/>
+  <Meta title="404" desc="404 page" noindex={true}/>
 </svelte:head>
 
 <div class="content">
-<h1 class="center">404</h1>
-<p class="center">Nope, no hints here!</p>
+  <h1 class="center">404</h1>
+  <p class="center">The requested URL was not found on this server.</p>
 </div>
