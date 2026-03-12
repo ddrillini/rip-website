fb06e6bcb9f9cdb155e672a7283793ef6de7bec3
andrewipark <andrewipark@users.noreply.github.com>
2026-03-12 09:38:11 -0500

    dev

--- src/routes/(app)/media/+page.svelte
+++ src/routes/(app)/media/+page.svelte
@@ -1,81 +1,103 @@
 <script lang="ts">
-  import globals from '../../../globals';
+	import globals from '../../../globals';
 	import Meta from '../../../components/layout/Meta.svelte';
 	import Link from '../../../components/Link.svelte';
-
 </script>
 
 <svelte:head>
-  <Meta title="Media" desc='Photos and other media taken at previous Rumble in the Prairie events.' noindex={false}/>
+	<Meta title="Media" desc='Photos and other media taken at previous Rumble in the Prairie events.' noindex={false}/>
 </svelte:head>
 
 <div class="content">
 <h1>Media</h1>
 
-<p class="comment">Some links point to unaffiliated services like Google Photos and Discord.</p>
+<p class="comment">Some links point to unaffiliated services like Google Photos™ and Discord®.</p>
 
 <h2>Grumble in the Pasture</h2>
-<p class="comment">February 2025</p>
+<p class="comment">2025-02</p>
 <ul>
-  <li> Mallow: <Link external href="https://immich.orca.flights/share/CS793kqAB05pTzq6E4sGGCKHRtL-a7RO-L9O0Sen7TO86i084haerrgERfibpyWFVRM" title="photos"/></li>
+	<li> Mallow: <Link external href="https://immich.orca.flights/share/CS793kqAB05pTzq6E4sGGCKHRtL-a7RO-L9O0Sen7TO86i084haerrgERfibpyWFVRM" title="photos"/></li>
 </ul>
 
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Why do we use it?
+</p>
+
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+</p>
+
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Why do we use it?
+</p>
+
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+</p>
+
 <h2>RIP 14</h2>
-<p class="comment">March 2024</p>
+<p class="comment">2024-03</p>
 <ul>
-  <li>Mallow: <Link external href="https://immich.orca.flights/share/zYURMeim0x6BpwGHMLJGxbjk9z0kJk0NUThHFv22MJ01khsu0rkbrPiD_ZgMyxUlLqk" title="photos"/></li>
-  <li>Solus: <Link external href="spw://UBFOV1ATDwwR" title="photos"/></li>
+	<li>Mallow: <Link external href="https://immich.orca.flights/share/zYURMeim0x6BpwGHMLJGxbjk9z0kJk0NUThHFv22MJ01khsu0rkbrPiD_ZgMyxUlLqk" title="photos"/></li>
+	<li>Solus: <Link external href="spw://UBFOV1ATDwwR" title="photos"/></li>
 </ul>
 
 <h2>RIP 13.5</h2>
-<p class="comment">November 2023</p>
+<p class="comment">2022-11</p>
 <ul>
-  <li>DDRIllini Discord:
-    <Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQsLCQ0NDwwIDQcHCgsICwcICBEPDwkLDA0NDg4PCQcMCQ8NCgwQ" title="photos"/>,
-    <Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQcODgoJCgkIBwcLEAoICw4QCxEPDwkMBwoQDA8QDgwICg8NCAcI" title="general RIP"/>
-  </li>
-  <li>Mallow: <Link external href="https://immich.orca.flights/share/l1qRyVWPPzL0k-Zud6UXkzvlO6-NElIFropV46ldAYcJYhdm7CDv77ZqB08CZ1V051U" title="photos"/></li>
-  <li>Solus: <Link external href="spw://UBFOV1ATDw0TCxE=" title="photos"/></li>
+	<li>DDRIllini Discord:
+		<Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQsLCQ0NDwwIDQcHCgsICwcICBEPDwkLDA0NDg4PCQcMCQ8NCgwQ" title="photos"/>,
+		<Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQcODgoJCgkIBwcLEAoICw4QCxEPDwkMBwoQDA8QDgwICg8NCAcI" title="general RIP"/>
+	</li>
+	<li>Mallow: <Link external href="https://immich.orca.flights/share/l1qRyVWPPzL0k-Zud6UXkzvlO6-NElIFropV46ldAYcJYhdm7CDv77ZqB08CZ1V051U" title="photos"/></li>
+	<li>Solus: <Link external href="spw://UBFOV1ATDw0TCxE=" title="photos"/></li>
 </ul>
 
 <h2>RIP 13</h2>
-<p class="comment">March 2023</p>
+<p class="comment">2023-03</p>
 <ul>
-  <li>DDRIllini Discord: <Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQsLCQ0NDwwIDQcHCgsICwcICBEPEAgMCgcHDQkKDA8ICwsMEA8K" title="photos"/></li>
-  <li>Mallow: <Link external href="https://immich.orca.flights/share/dEglOk8KFcDjMwQOVjafSzE6TAnqD5Qqws_sxEAehbt-DN6bNqN0KdG-WmcaOaxh7UE" title="photos"/></li>
-  <li>Solus: <Link external href="spw://UBFOV1ATDw0TEBE" title="photos"/></li>
-  <li>Talkion: <Link external href="gp://SwtPCXVYS1ZQdFNcblxZSAo=" title="selfies"/></li>
-  <li>Tyler F., Devin: <Link external href="gp://Vm5RDktvX115aAdfe3tOSQc=" title="photos"/></li>
+	<li>DDRIllini Discord: <Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQsLCQ0NDwwIDQcHCgsICwcICBEPEAgMCgcHDQkKDA8ICwsMEA8K" title="photos"/></li>
+	<li>Mallow: <Link external href="https://immich.orca.flights/share/dEglOk8KFcDjMwQOVjafSzE6TAnqD5Qqws_sxEAehbt-DN6bNqN0KdG-WmcaOaxh7UE" title="photos"/></li>
+	<li>Solus: <Link external href="spw://UBFOV1ATDw0TEBE" title="photos"/></li>
+	<li>Talkion: <Link external href="gp://SwtPCXVYS1ZQdFNcblxZSAo=" title="selfies"/></li>
+	<li>Tyler F., Devin: <Link external href="gp://Vm5RDktvX115aAdfe3tOSQc=" title="photos"/></li>
 </ul>
 
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Why do we use it?
+</p>
+
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+</p>
+
 <h2>RIP 12.5</h2>
-<p class="comment">November 2022</p>
+<p class="comment">2022-11</p>
 <ul>
-  <li>DDRIllini Discord: <Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQsLCQ0NDwwIDQcHCgsICwcICBEPEAwNCwkPCgcMDgsLCQsPDg8I" title="photos"/></li>
-  <li>Mallow: <Link external href="https://immich.orca.flights/share/40oR-YCqYxD_oZVvLw0V1VpJ53btTWbfcfA_5mdM00OEHjJR_6aj1OstAZP2vmX3sqw" title="photos"/>, <Link external href="yt://dE9ScHJ6XFN6E1k=" title="B-roll"/></li>
-  <li>PSU collab: <Link external href="gp://bVVvc2l+ZklrXGxLWE9XTAg=" title="photos"/></li>
-  <li>Solus: <Link external href="spw://UBFOV1ATDw4TCxE" title="photos"/></li>
+	<li>DDRIllini Discord: <Link external href="discord://XVhfUlJbVE0RDg8NCQ8MCQwHDA8KBw0PDQ4HEQsLCQ0NDwwIDQcHCgsICwcICBEPEAwNCwkPCgcMDgsLCQsPDg8I" title="photos"/></li>
+	<li>Mallow: <Link external href="https://immich.orca.flights/share/40oR-YCqYxD_oZVvLw0V1VpJ53btTWbfcfA_5mdM00OEHjJR_6aj1OstAZP2vmX3sqw" title="photos"/>, <Link external href="yt://dE9ScHJ6XFN6E1k=" title="B-roll"/></li>
+	<li>PSU collab: <Link external href="gp://bVVvc2l+ZklrXGxLWE9XTAg=" title="photos"/></li>
+	<li>Solus: <Link external href="spw://UBFOV1ATDw4TCxE" title="photos"/></li>
 </ul>
 
 <h2>RIP 12.1</h2>
-<p class="comment">March 2022</p>
+<p class="comment">2022-03</p>
 <ul>
-  <li>Mallow: <Link external href="https://immich.orca.flights/share/55iUA7i8neJePzcSW7P4Sd2E3rbBYw48NXY75inSdWQ-tREcmcmDGOT8pWJbW17L8iY" title="photos"/></li>
-  <li>PSU collab: <Link external href="gp://U0xrRwxISVl9eHZ8S1BabAc=" title="photos"/></li>
-  <li>Solus: <Link external href="spw://UBFOV1ATDw4TDxE" title="photos"/></li>
+	<li>Mallow: <Link external href="https://immich.orca.flights/share/55iUA7i8neJePzcSW7P4Sd2E3rbBYw48NXY75inSdWQ-tREcmcmDGOT8pWJbW17L8iY" title="photos"/></li>
+	<li>PSU collab: <Link external href="gp://U0xrRwxISVl9eHZ8S1BabAc=" title="photos"/></li>
+	<li>Solus: <Link external href="spw://UBFOV1ATDw4TDxE" title="photos"/></li>
 </ul>
 
-<p>RIP 12 would have started on March 2020-13, but was cancelled on March 2020-12 due to the COVID-19 pandemic.</p>
+<p>RIP 12 would have started on 2020-03-13, but was cancelled on 2020-03-12 due to the COVID-19 pandemic.</p>
+
 <h2>Older Events</h2>
 <ul>
-  <li><Link external href="https://immich.orca.flights/share/aaSXYjc18UoCfim_5w2AM3B1AikueY1G6N_fKjaVqycRuuRnbKquR3ywuXb_i9ORdF4" title="RIP 11.5, Mallow"/></li>
-    <li><Link external href="https://immich.orca.flights/share/-5EdZLyoosdB-SNkvwwGWkGfn69t3I_z2drSakVk0luIjr8YHU4xwfaNblVpaai4H2E" title="RIP 11, Mallow"/></li>
-    <li><Link external href="gp://bE5STHtdfG18XktebHh+Ugg=" title="RIP 10, quietly-turning"/></li>
-    <li><Link external href="gp://XU4PXH2TB111eGpoewx4Wn8=" title="RIP 9, quietly-turning"/></li>
-    <li><Link external href="gp://aAdzTH9mW2hmCXhraV9QZwk=" title="RIP 8.5, quietly-turning"/></li>
-    <li><Link external href="gp://DUoORkx8DQwKbmhmZlAKUgk=" title="RIP 7.5, quietly-turning"/></li>
-    <li><Link external href="gp://ZlFvWnJ7b2cHDA1reGdKbwk=" title="RIP 5.5, quietly-turning"/></li>
+	<li><Link external href="https://immich.orca.flights/share/aaSXYjc18UoCfim_5w2AM3B1AikueY1G6N_fKjaVqycRuuRnbKquR3ywuXb_i9ORdF4" title="RIP 11.5, Mallow"/></li>
+	<li><Link external href="https://immich.orca.flights/share/-5EdZLyoosdB-SNkvwwGWkGfn69t3I_z2drSakVk0luIjr8YHU4xwfaNblVpaai4H2E" title="RIP 11, Mallow"/></li>
+	<li><Link external href="gp://bE5STHtdfG18XktebHh+Ugg=" title="RIP 10, quietly-turning"/></li>
+	<li><Link external href="gp://XU4PXH2TB111eGpoewx4Wn8=" title="RIP 9, quietly-turning"/></li>
+	<li><Link external href="gp://aAdzTH9mW2hmCXhraV9QZwk=" title="RIP 8.5, quietly-turning"/></li>
+	<li><Link external href="gp://DUoORkx8DQwKbmhmZlAKUgk=" title="RIP 7.5, quietly-turning"/></li>
+	<li><Link external href="gp://ZlFvWnJ7b2cHDA1reGdKbwk=" title="RIP 5.5, quietly-turning"/></li>
 </ul>
 
 <p>Thank you.</p>
+
 </div>
